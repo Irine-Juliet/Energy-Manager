d84fa060654ac6f6edce88c22e38f0b26db1e2f5
sophie-gaoyang <sophie.gaoyang@gmail.com>
2025-11-18 09:31:54 -0500

    Resolve merge conflicts in models.py and views.py

--- energy_tracker/models.py
+++ energy_tracker/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Activity(models.Model):
